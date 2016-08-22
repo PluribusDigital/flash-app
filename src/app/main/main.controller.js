a1d2bf109465e01323b5e4b76ebc9730e3ee3659
Cullen Kirkpatrick <cullen@540.co>
2016-08-22 19:19:53 -0400

    Adding Create Kudo Modal UI

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,9 +6,11 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController($log, $rootScope) {
     var vm = this;
     vm.userStats = { total: 18, given:8, received:10 };
+    vm.kudo = {};
+    vm.giveKudo = giveKudo;
     vm.mockNotifications = [
       {
         type: "Request",
@@ -26,6 +28,14 @@
         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       }
     ];
+
+    function giveKudo(kudo){
+      //kudoService.kudos.addKudo().then(function(){
+      //   vm.createKudo = false;
+      //});
+      vm.createKudo = false;
+      kudo.from = $rootScope.loggedInUser.name;
+    }
 
   }
 })();
